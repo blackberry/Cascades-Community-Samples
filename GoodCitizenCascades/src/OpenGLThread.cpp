c5ed15eff5090d7521b600adfba5cac7fa750ad0
gvording <gvording@rim.com>
2013-02-08 11:54:05 -0500

    Explicitly request depth buffer

--- GoodCitizenCascades/src/OpenGLThread.cpp
+++ GoodCitizenCascades/src/OpenGLThread.cpp
@@ -191,6 +191,7 @@ int OpenGLThread::initEGL() {
     EGLint attrib_list[]= { EGL_RED_SIZE,        8,
                             EGL_GREEN_SIZE,      8,
                             EGL_BLUE_SIZE,       8,
+                            EGL_DEPTH_SIZE, 	 16,
                             EGL_SURFACE_TYPE,    EGL_WINDOW_BIT,
                             EGL_RENDERABLE_TYPE, EGL_OPENGL_ES_BIT,
                             EGL_NONE};
