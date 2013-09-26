22188e4c902b2e2ea338cb3f4b0117ff5d31e3bf
Isaac Gordezky <igordezky@blackberry.com>
2013-09-26 10:41:47 -0400

    added an example of library assets simplified the renaming process make the linker throw an error on undefined symbols readme updates remove license as this is a template

--- Cascades-Library-Template/MyLibrary/src/MyLibrary.cpp
+++ Cascades-Library-Template/MyLibrary/src/MyLibrary.cpp
@@ -1,19 +1,8 @@
-/* Copyright (c) 2013 Research In Motion Limited.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
+// Default empty project template
 #include <bb/cascades/Container>
+#include <bb/cascades/QmlDocument>
+#include <bb/cascades/NavigationPane>
+#include <bb/cascades/Button>
 #include <bb/cascades/Color>
 #include <bb/cascades/Label>
 #include <bb/cascades/SystemDefaults>
@@ -32,12 +21,25 @@ MyLibrary::MyLibrary(QObject *parent)
 		.vertical(VerticalAlignment::Center).horizontal(HorizontalAlignment::Center)
 		.text("Hello from MyLibrary");
 
-	Container * c = Container::create().parent(this).background(Color::LightGray).add(lbl);
+	Button * btn = Button::create().text("Test Library QML").onClicked(this, SLOT(onButtonClicked()));
+
+	Container * c = Container::create().parent(this).background(Color::LightGray).add(lbl).add(btn);
 	Page::setContent(c);
+
+	QmlDocument *qml = QmlDocument::create("asset:///lib.MyLibrary/MyPage.qml").parent(this);
+
+	qmlPage = qml->createRootObject<Page>();
 }
 
 MyLibrary::~MyLibrary() {
 	// TODO Auto-generated destructor stub
+}
+
+void MyLibrary::onButtonClicked() {
+	NavigationPane * navPane = qobject_cast<NavigationPane*>(parent());
+	qDebug() << "onButtonClicked()" << navPane;
+	if (navPane)
+		navPane->push(qmlPage);
 }
 
 } /* end namespace custom */
