69018c6d576f6bcf87bc6d18492f6a27d6929117
Martin Woolley <mwoolley@blackberry.com>
2013-10-01 09:16:14 +0100

    New application which queries the Bluetooth GATT Device Information Service on a Bluetooth Low Energy device

---
+++ BtDeviceInfo/src/DeviceListing.hpp
@@ -0,0 +1,48 @@
+/* Copyright (c) 2013 BlackBerry Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DEVICELISTING_HPP
+#define DEVICELISTING_HPP
+
+#include <QObject>
+
+#include <bb/cascades/GroupDataModel>
+
+#include <bb/system/SystemDialog>
+#include <bb/system/SystemToast>
+
+#include <btapi/btdevice.h>
+#include <btapi/btspp.h>
+
+class DeviceListing : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bb::cascades::DataModel* model READ model CONSTANT)
+
+public:
+    DeviceListing(QObject *parent = 0);
+
+public Q_SLOTS:
+    void update();
+    void discover();
+
+private:
+    bb::cascades::DataModel* model() const;
+    bb::cascades::GroupDataModel* _model;
+    bool hasDeviceInformationService(bt_remote_device_t * remoteDevice);
+    QString DEVICE_INFORMATION_SERVICE_UUID;
+};
+
+#endif
---
+++ BtDeviceInfo/translations/BtDeviceInfo.ts
@@ -0,0 +1,339 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>DeviceListing</name>
+    <message>
+        <location filename="../src/DeviceListing.cpp" line="66"/>
+        <source>Paired Bluetooth Devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/DeviceListing.cpp" line="83"/>
+        <source>Searching for Bluetooth LE devices ... please wait until search has completed ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/DeviceListing.cpp" line="91"/>
+        <source>Search completed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/DeviceListing.cpp" line="122"/>
+        <source>Bluetooth Devices Nearby</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LocalDeviceInfo</name>
+    <message>
+        <location filename="../src/LocalDeviceInfo.cpp" line="33"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocalDeviceInfo.cpp" line="34"/>
+        <source>N/A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocalDeviceInfo.cpp" line="49"/>
+        <location filename="../src/LocalDeviceInfo.cpp" line="50"/>
+        <source>true</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocalDeviceInfo.cpp" line="49"/>
+        <location filename="../src/LocalDeviceInfo.cpp" line="50"/>
+        <source>false</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocalDeviceInfo.cpp" line="53"/>
+        <source>Low energy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocalDeviceInfo.cpp" line="53"/>
+        <source>Regular</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RemoteDeviceInfo</name>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="42"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="43"/>
+        <source>N/A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="61"/>
+        <source>Low energy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="61"/>
+        <source>Regular</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="64"/>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="69"/>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="72"/>
+        <source>true</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="64"/>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="69"/>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="72"/>
+        <source>false</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="104"/>
+        <source>Bluetooth BR/EDR Services</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="121"/>
+        <source>Bluetooth LE GATT Services</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="175"/>
+        <source>SDP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="177"/>
+        <source>RFCOMM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="179"/>
+        <source>OBEX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="181"/>
+        <source>HTTP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="183"/>
+        <source>L2CAP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="185"/>
+        <source>BNEP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="187"/>
+        <source>Service Discovery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="189"/>
+        <source>Browse Group Descriptor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="191"/>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="195"/>
+        <source>Public Browse Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="193"/>
+        <source>Serial Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="197"/>
+        <source>OBEX Object Push</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="199"/>
+        <source>OBEX File Transfer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="201"/>
+        <source>Personal Area Networking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="203"/>
+        <source>Network Access Point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="205"/>
+        <source>Group Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="207"/>
+        <source>Generic Access</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="209"/>
+        <source>Generic Attribute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="211"/>
+        <source>ImmediateAlert</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="213"/>
+        <source>Link Loss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="215"/>
+        <source>Tx Power</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="217"/>
+        <source>Current Time Service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="219"/>
+        <source>Reference Time Update Service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="221"/>
+        <source>Next DST Change Service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="223"/>
+        <source>Glucose</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="225"/>
+        <source>Health Thermometer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="227"/>
+        <source>Device Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="229"/>
+        <source>Heart Rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="231"/>
+        <source>:Phone Alert Status Service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="233"/>
+        <source>Battery Service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="235"/>
+        <source>Blood Pressure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="237"/>
+        <source>Alert Notification Service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="239"/>
+        <source>Human Interface Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="241"/>
+        <source>Scan Parameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="243"/>
+        <source>Running Speed and Cadance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RemoteDeviceInfo.cpp" line="245"/>
+        <source>Other</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="37"/>
+        <source>
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="54"/>
+        <source>Bluetooth Device Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="46"/>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="111"/>
+        <source>Bluetooth Devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="182"/>
+        <source>About BtDeviceInfo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="189"/>
+        <source>V1.0.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="194"/>
+        <source>Works with the Bluetooth Device Info Profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="207"/>
+        <source>Authors:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="220"/>
+        <source>Martin Woolley (@mdwrim)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="233"/>
+        <source>John Murray (@jcmrim)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
