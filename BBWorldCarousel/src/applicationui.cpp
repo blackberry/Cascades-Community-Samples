34f500a4338b320720f28a2cec8c0e9c1cf76af8
DiegoAnfuso <diegoanfuso@gmail.com>
2013-10-04 14:08:43 -0300

    Initial commit

---
+++ BBWorldCarousel/src/applicationui.cpp
@@ -0,0 +1,46 @@
+#include "applicationui.hpp"
+
+#include <bb/cascades/Application>
+#include <bb/cascades/QmlDocument>
+#include <bb/cascades/AbstractPane>
+#include <bb/cascades/LocaleHandler>
+
+using namespace bb::cascades;
+
+ApplicationUI::ApplicationUI(bb::cascades::Application *app) :
+        QObject(app)
+{
+    // prepare the localization
+    m_pTranslator = new QTranslator(this);
+    m_pLocaleHandler = new LocaleHandler(this);
+    if(!QObject::connect(m_pLocaleHandler, SIGNAL(systemLanguageChanged()), this, SLOT(onSystemLanguageChanged()))) {
+        // This is an abnormal situation! Something went wrong!
+        // Add own code to recover here
+        qWarning() << "Recovering from a failed connect()";
+    }
+    // initial load
+    onSystemLanguageChanged();
+
+    qmlRegisterType<QTimer>("my.library", 1, 0, "QTimer");
+
+    // Create scene document from main.qml asset, the parent is set
+    // to ensure the document gets destroyed properly at shut down.
+    QmlDocument *qml = QmlDocument::create("asset:///main.qml").parent(this);
+
+    // Create root object for the UI
+    AbstractPane *root = qml->createRootObject<AbstractPane>();
+
+    // Set created root object as the application scene
+    app->setScene(root);
+}
+
+void ApplicationUI::onSystemLanguageChanged()
+{
+    QCoreApplication::instance()->removeTranslator(m_pTranslator);
+    // Initiate, load and install the application translation files.
+    QString locale_string = QLocale().name();
+    QString file_name = QString("BBWorldCarousel_%1").arg(locale_string);
+    if (m_pTranslator->load(file_name, "app/native/qm")) {
+        QCoreApplication::instance()->installTranslator(m_pTranslator);
+    }
+}
