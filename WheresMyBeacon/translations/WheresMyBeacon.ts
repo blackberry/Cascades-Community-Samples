13eaf2e54a8eee5be5d642730c37d6d90b5881cf
John Murray <jomurray@blackberry.com>
2014-04-29 10:41:17 +0100

    Missing "About" page and corrected layout of beacon data in ListView

--- WheresMyBeacon/translations/WheresMyBeacon.ts
+++ WheresMyBeacon/translations/WheresMyBeacon.ts
@@ -2,15 +2,48 @@
 <!DOCTYPE TS>
 <TS version="2.0">
 <context>
+    <name>AboutSheet</name>
+    <message>
+        <location filename="../assets/AboutSheet.qml" line="59"/>
+        <source>About WheresMyBeacon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/AboutSheet.qml" line="84"/>
+        <source>V1.0.1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/AboutSheet.qml" line="138"/>
+        <source>The WheresMyBeacon application demonstrates how to use iBeacon technology from a BlackBerry 10 application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/AboutSheet.qml" line="145"/>
+        <source>Authors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/AboutSheet.qml" line="149"/>
+        <source>John Murray (@jcmrim)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/AboutSheet.qml" line="154"/>
+        <source>Martin Woolley (@mdwrim)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
-        <location filename="../assets/main.qml" line="43"/>
+        <location filename="../assets/main.qml" line="65"/>
         <source>
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../assets/main.qml" line="54"/>
+        <location filename="../assets/main.qml" line="76"/>
         <source>Where&apos;s My Beacon</source>
         <translation type="unfinished"></translation>
     </message>
